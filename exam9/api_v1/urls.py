e83d1d8ce31e4b47ede133baf5ca318375b9e21f
Ermek K <kadyrkulove@gmail.com>
2019-04-06 16:10:58 +0600

    added urls

--- exam9/api_v1/urls.py
+++ exam9/api_v1/urls.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env bash
-
 from django.urls import include, path
 from rest_framework import routers
 from django.contrib import admin
 from api_v1 import views
 
 
-# создаём объект router, который привязывает ViewSet к путям на сайте
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'products', views.ProductViewSet)
+router.register(r'photos', views.ProductPhotoViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'orders', views.OrderViewSet)
 
 app_name = 'api_v1'
 
@@ -18,6 +19,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.UserCreateView.as_view(), name='register'),
     path('token-login/', views.TokenLoginView.as_view(), name='api_token_re_login'),
-    # новая точка входа, куда можно прислать POST-запрос с токеном для активации нового пользователя
     path('register/activate/', views.UserActivateView.as_view(), name='register_activate')
 ]
