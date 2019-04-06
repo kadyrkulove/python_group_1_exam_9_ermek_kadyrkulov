930718441fc5f6f77cf39719c5a1847e7cbf2ce7
Ermek K <kadyrkulove@gmail.com>
2019-04-06 20:20:41 +0600

    Added ProductCard.js, Card.js

--- frontend/src/components/UI/Card/Card.js
+++ frontend/src/components/UI/Card/Card.js
@@ -1,1 +1,18 @@
-import React
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+
+const Card = props => {
+    return <div className={"card mt-3 text-center text-sm-left " + (props.className ? props.className : "")}>
+        {props.image ? <img className="card-img-top" src={props.image} alt="poster"/> : null}
+        {props.header || props.text || props.link ? <div className="card-body">
+            {props.header ? <h5 className="card-title">{props.header}</h5> : null}
+            {props.text ? <p className="card-text">{props.text}</p> : null}
+            {props.link ? <NavLink to={props.link.url} className="btn btn-primary mr-2">
+                {props.link.text}
+            </NavLink> : null}
+        </div> : null}
+    </div>
+};
+
+
+export default Card;
