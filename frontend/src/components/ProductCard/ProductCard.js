96a6eae466ba91186d75c24ea423d922dfbcd927
Ermek K <kadyrkulove@gmail.com>
2019-04-06 18:57:03 +0600

    add product list

--- frontend/src/components/ProductCard/ProductCard.js
+++ frontend/src/components/ProductCard/ProductCard.js
@@ -1,2 +1,22 @@
 import React from 'react';
-import Card from
+// import Card from "../UI/Card/Card";
+
+const MovieCard = props => {
+    const {product} = props;
+
+    let photo = "http://localhost:8000/uploads/default_image.png";
+    if (product.photos[0]) {
+        photo = product.photos[0].photo
+    }
+
+    const {name, id} =product;
+
+    const link = {
+        text: 'more',
+        url: '/products/' + id,
+    };
+
+    return <Card header={name} image={photo} link={link} className='h-100'/>;
+};
+
+export default MovieCard;
